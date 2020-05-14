97eef2cc057d5ba0a129069fdc217cbe2a83b1d0
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2020-05-14 22:32:02 +0200

    Injected the FoodItem DAO into the controller.

--- src/com/luv2code/springdemo/controller/FoodItemController.java
+++ src/com/luv2code/springdemo/controller/FoodItemController.java
@@ -1,18 +1,31 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.luv2code.springdemo.dao.FoodItemDAO;
+import com.luv2code.springdemo.entity.FoodItem;
+
 @Controller
 @RequestMapping("/food-item")
 public class FoodItemController {
+	
+	@Autowired
+	private FoodItemDAO foodItemDAO;
 
 	@RequestMapping("/list")
 	public String listFoodItems(Model theModel) {
 		
+		List<FoodItem> theFoodItems = foodItemDAO.getFoodItems();
+		
+		theModel.addAttribute("foodItems", theFoodItems);
+		
+		
 		return "list-food-items";
 	}
-	
 	
 }
