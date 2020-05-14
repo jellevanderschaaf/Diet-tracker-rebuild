fc49cab7518fa617ca44c51f8f81f3913b48d1b8
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2020-05-14 22:22:29 +0200

    Added the DAO implementation for FoodItem.

---
+++ src/com/luv2code/springdemo/dao/FoodItemDAOImpl.java
@@ -0,0 +1,34 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.FoodItem;
+
+@Repository
+public class FoodItemDAOImpl implements FoodItemDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<FoodItem> getFoodItems() {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<FoodItem> theQuery = currentSession.createQuery("from FoodItem", FoodItem.class);
+		
+		List<FoodItem> foodItems = theQuery.getResultList();
+		
+		return foodItems;
+		
+	}
+
+}
